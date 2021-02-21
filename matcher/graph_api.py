3fb9b0e42154f11232d4ff0aab7b373652ddb88e
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-02-21 13:52:21 +0000

    just working for simple queries such as: "REGISTER QUERY my_first_query OUTPUT K_GRAPH_JSON CONTENT ObjectDetection MATCH (c:car)-[:same_frame]-(p:person)-[:same_frame]-(p2:person) FROM publisher1 WITHIN TUMBLING_COUNT_WINDOW(30) RETURN *" With the above stats: Total nodes: 1614 Total windows: 10 avg nodes p/ window: 161.4 num events per window = 30 5.38/frame 30 frames/window 10 windows / 0.4623546 348/sec nodes (161.4/0.4623546)

--- matcher/graph_api.py
+++ matcher/graph_api.py
@@ -5,42 +5,61 @@ class RedisGraphDB():
     def __init__(self, redis_graph_db):
         self.redis_graph_db = redis_graph_db
         self.query_graphs = {}
+        self.query_graphs_nodes = {}
 
     def _clean_identifier(self, identifier):
         if ' ' in identifier:
             return f'`{identifier}`'
         return identifier
 
-    def add_vekg_nodes_to_graph(self, graph, event_id, note_tuples):
-        for node_id, properties in note_tuples:
+    def add_vekg_nodes_to_graph(self, query_id, graph, event_id, node_tuples):
+        for node_id, properties in node_tuples:
             node_kwargs = properties.copy()
             node_kwargs['event_id'] = event_id
             label = self._clean_identifier(node_kwargs.pop('label'))
-            new_node = Node(label=label, properties=properties)
+            node_id = node_kwargs.pop('id')
+            new_node = Node(node_id=node_id, label=label, properties=properties)
+            self.query_graphs_nodes[query_id][node_id] = new_node
             graph.add_node(new_node)
         return graph
 
-    def add_vekg_edges_to_graph(self, graph, edges_tuples):
-        for edge in edges_tuples:
-            node_u, node_v, properties = edge
-            edges_kwargs = properties.copy()
-            relation = self._clean_identifier(edges_kwargs.pop('relation'))
-            graph.add_edge(node_u, node_v, relation=relation, properties=properties)
+    def add_vekg_edges_to_graph(self, query_id, graph, edges_tuples):
+        for edge_tuple in edges_tuples:
+            node_u_id, node_v_id, properties = edge_tuple
+            edge_kwargs = properties.copy()
+            relation = self._clean_identifier(edge_kwargs.pop('relation'))
+            node_u = self.query_graphs_nodes[query_id][node_u_id]
+            node_v = self.query_graphs_nodes[query_id][node_v_id]
+            edge = Edge(node_u, relation, node_v, properties=edge_kwargs)
+            graph.add_edge(edge)
         return graph
 
-    def add_vekg_to_graph(self, graph, event_id, vekg):
-        note_tuples = vekg.get('nodes', [])
-        graph = self.add_vekg_nodes_to_graph(graph, event_id, note_tuples)
+    def add_same_frame_rel_edges(self, graph, event_id, vekg):
+        node_tuples = vekg.get('nodes', [])
+        same_frames_relations = []
+        for node_u in node_tuples:
+            for node_v in node_tuples:
+                if node_u[0] != node_v[0]:
+                    relation = 'same_frame'
+                    edge_tuple = (node_u[0], node_v[0], {'relation': relation})
+                    same_frames_relations.append(edge_tuple)
         edge_tuples = vekg.get('edges', [])
-        graph = self.add_vekg_edges_to_graph(graph, edge_tuples)
+        return edge_tuples + same_frames_relations
+
+    def add_vekg_to_graph(self, query_id, graph, event_id, vekg):
+        node_tuples = vekg.get('nodes', [])
+        graph = self.add_vekg_nodes_to_graph(query_id, graph, event_id, node_tuples)
+        edge_tuples = self.add_same_frame_rel_edges(graph, event_id, vekg)
+        graph = self.add_vekg_edges_to_graph(query_id, graph, edge_tuples)
         return graph
 
     def add_query_vekg_window(self, query_id, vekg_window):
         query_graph = Graph(query_id, self.redis_graph_db)
+        self.query_graphs_nodes[query_id] = {}
         for event in vekg_window:
             event_id = event['id']
             vekg = event.get('vekg', {})
-            query_graph = self.add_vekg_to_graph(query_graph, event_id, vekg)
+            query_graph = self.add_vekg_to_graph(query_id, query_graph, event_id, vekg)
         query_graph.commit()
         self.query_graphs[query_id] = query_graph
         return self.query_graphs[query_id]
@@ -55,5 +74,6 @@ class RedisGraphDB():
         try:
             self.query_graphs[query_id].delete()
             del self.query_graphs[query_id]
+            del self.query_graphs_nodes[query_id]
         except:
             pass
